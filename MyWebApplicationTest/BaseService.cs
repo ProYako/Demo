df73f3b0add704f1696d6a415c7aae5ad58c0f37
yakolive <proyaya8@gmail.com>
2020-12-12 15:22:59 +0800

    簡單RetrieveOrder測試

---
+++ MyWebApplicationTest/BaseService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using MyWebApplication.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyWebApplicationTest
+{
+    public class BaseService
+    {
+
+        public void DoAssert(Object expect, Object actual)
+        {
+            Assert.Equal(JsonConvert.SerializeObject(expect), JsonConvert.SerializeObject(actual));
+        }
+
+        public mytestdatabase12Context CreateNewContext()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<mytestdatabase12Context>(c =>
+                c.UseInMemoryDatabase(Guid.NewGuid().ToString()).ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning)));//.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+
+            var serviceProvider = services.BuildServiceProvider();
+            var dbContext = serviceProvider.GetRequiredService<mytestdatabase12Context>();
+
+            return dbContext;
+        }
+
+        public void SetBasicTestData(mytestdatabase12Context dbContext) 
+        {
+            //Order order01 = CreateDbOrder(1, "測試人01", new DateTime(2020, 12, 01));
+            //Order order02 = CreateDbOrder(2, "測試人02", new DateTime(2020, 12, 02));
+            //Order order03 = CreateDbOrder(3, "測試人03", new DateTime(2020, 12, 03));
+
+            //dbContext.AddRange(order01, order02, order03);
+        }
+
+        public Order CreateDbOrder(int orderId, string customerId, DateTime orderDate)
+        {
+            Order order = new Order
+            {
+                OrderId = orderId,
+                CustomerId = customerId,
+                OrderDate = orderDate
+            };
+
+            return order;
+        }
+    }
+}
---
+++ MyWebApplicationTest/OrderServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using MyWebApplication.Models;
+using MyWebApplication.Service;
+using MyWebApplication.Service.Interface;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyWebApplicationTest
+{
+    
+    public class OrderServiceTest
+    {
+        private BaseService creator { get; set; }
+
+        public OrderServiceTest()
+        {
+            creator = new BaseService();
+        }
+
+
+        [Fact]
+        public async Task TestRetrieveOrder()
+        {
+            #region db測試資料
+            mytestdatabase12Context dbContext = creator.CreateNewContext();
+            creator.SetBasicTestData(dbContext);
+
+            Order order01 = creator.CreateDbOrder(1, "測試人01", new DateTime(2020, 12, 01));
+            Order order02 = creator.CreateDbOrder(2, "測試人02", new DateTime(2020, 12, 02));
+            Order order03 = creator.CreateDbOrder(3, "測試人03", new DateTime(2020, 12, 03));
+
+            dbContext.AddRange(order01, order02, order03);
+
+            await dbContext.SaveChangesAsync();
+            #endregion
+
+            #region Actual Test Func
+            IOrderService _orderService = new OrderService(dbContext);
+            string testCustomerId = "測試人01";
+            List<Order> actualResult = await _orderService.RetrieveOrder(testCustomerId);
+            #endregion
+
+            #region Expected
+            List<Order> expectResult = new List<Order>()
+            {
+                new Order()
+                {
+                    OrderId = 1,
+                    CustomerId = "測試人01",
+                    OrderDate = new DateTime(2020, 12, 01)
+                }
+            };
+            #endregion
+
+            //驗證
+            creator.DoAssert(expectResult, actualResult);
+        }
+    }
+}
